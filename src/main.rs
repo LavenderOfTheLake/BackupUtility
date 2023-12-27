fbdd6ee229ca29875e75a504b75dd88795b2b112
Anabelle <git@anabelle.dev>
2023-12-27 07:56:19 -0500

    v0.2

--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,76 @@
-pub mod types; 
+pub mod types;
+
 use std::{fs::DirBuilder, path::PathBuf};
 use std::path::Path;
-
 use types::{Snapshot, Vault, Volume};
 
-
-/// # Take_Snapshot
-/// ## Preconditions
-///     vol is valid 
-///     user is root or has perms
-/// 
-/// 
-/// ## Example executed command
-///     `sudo btrfs subvol snap /home/anabelle/ . /mnt/@snapshots/1`
-/// ## TODO
-///     make it use the local timezone
-fn snap_volume<'a,'b>(
-    vol: &'a Volume, 
+fn snap<'a>(
     vault: &'a Vault
 ) -> Result<Snapshot<'a>,String> {
 
+    //Stage 1 - verify that all relevant paths are btrfs subvols
     if ! is_btrfs_subvol(vault.path.as_path()) {
-        let err_text = format!("Vault Path {:?} is not a valid btrfs subvolume",vault.path);
-        return Err(err_text);
+        return Err(format!(
+            "Vault Path {:?} is not a valid btrfs subvolume",
+            vault.path
+        ));
     }
 
-    // for vol in vault.volumes.clone() {
-        if ! is_btrfs_subvol(vol.path.as_path()) {
-            return Err(format!("Subvolume Path {:?} is not a valid btrfs subvolume",vol.path))
+    for vol in vault.volumes.clone() {
+        if !is_btrfs_subvol(vol.path.as_path()) {
+            return Err(format!(
+                "Subvolume Path {:?} is not a valid btrfs subvolume",
+                vol.path
+            ));
         }
-    // }
+    }
 
+    //Stage 2 - create the btrfs snapshot folder
     let snap_time = chrono::Local::now();
-
-    let snap_time_folder: PathBuf = snap_time
+    let snap_folder: PathBuf = snap_time //relative path
         .format("%F %I:%M%p")
         .to_string()
         .into();
-        
-    let snap_time_folder = vault.path
-        .join(snap_time_folder);
+    let snap_folder = vault.path //absolute path
+        .join(snap_folder);
 
-    println!("creating snapshot dir {:?}",snap_time_folder);
-    DirBuilder::new()
-        .recursive(true)
-        .create(&snap_time_folder)
-        .expect("Dirbuilder should never panic when recursive=true");
+    println!("creating snapshot dir {:?}",snap_folder);
 
-    let source = vol.path.as_path();
+    match {
+        DirBuilder::new()
+            .recursive(true)
+            .create(&snap_folder) 
+    } {
+        Err(_) => {
+            return Err("Couldn't create Snapshot folder".into());
+        },
+        Ok(_) => (),
+    };
 
-    let destination = &snap_time_folder
-        .join(&vol.name);
     
-    let _proc = std::process::Command::new("btrfs")
-        .arg("subvolume")
-        .arg("snapshot")
-        .arg(source)
-        .arg(destination)
-        .output();
-
-    println!("running subvolume snapshot {:?} {:?}",&source,&destination);
-
-    match  _proc {
-        Err(_) => Err("btrfs subvol snapshot failed".into()),
-        Ok (_) => Ok(Snapshot{time:snap_time.naive_local(),vault}),
+    //stage 3 - create snapshots
+    for vol in vault.volumes.clone() {
+        let source = vol.path.as_path();
+        let destination = &snap_folder.join(&vol.name);
+                
+        let result = 
+            std::process::Command::new("btrfs")
+            .arg("subvolume")
+            .arg("snapshot")
+            .arg(&source)
+            .arg(&destination)
+            .output();
+        
+        match result {
+            Err(err) => return Err(err.to_string()),
+            Ok(_) => (),
+        }
     }
-
+    
+    Ok(Snapshot{
+        vault,
+        time: snap_time.naive_local()
+    })
 }
 
 fn main() {
@@ -74,8 +79,8 @@ fn main() {
         path: "/home".into()
     };
 
-    snap_volume(
-        &v, &Vault {
+    snap(
+        &Vault {
             path: "/mnt/@snapshots".into(),
             retain_number: 0,
             snapshots: Vec::new(),
